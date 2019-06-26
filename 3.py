18e64939ad845571bc4be80a86fbd89aed57bc72
GOWRI181109 <52199117+GOWRI181109@users.noreply.github.com>
2019-06-26 15:26:01 +0530

    Create 3.py

---
+++ 3.py
@@ -0,0 +1,8 @@
+ch = input()
+if ch.isalpha():
+    if(ch=='A' or ch=='a' or ch=='E' or ch =='e' or ch=='I'or ch=='i' or ch=='O' or ch=='o' or ch=='U' or ch=='u'):
+        print(ch, "is a Vowel")
+    else:
+        print(ch, "is a Consonant")
+else:
+    print("invalid")
